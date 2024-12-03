f25cabfa1f66127e52263a0192cf6dd169f8e50a
Marek Jagoš <jagos.marek@outlook.cz>
2024-12-03 17:46:30 +0100

    Add DB Java Driver dependencies

--- app/src/main/java/com/github/mjagos0/polyglot_project/App.java
+++ app/src/main/java/com/github/mjagos0/polyglot_project/App.java
@@ -1,8 +1,5 @@
 package com.github.mjagos0.polyglot_project;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
